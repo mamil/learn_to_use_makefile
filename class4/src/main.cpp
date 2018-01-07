4b2a40c89f17178426e01fbbbdae3435e4c65c3b
hakunamatata <maogithub@163.com>
2018-01-07 10:41:13 +0800

    让组织更加正规化，Makefile更加智能
    增加目录结构，让Makefile自己找到要生成的目标，更加便于使用

---
+++ class4/src/main.cpp
@@ -0,0 +1,9 @@
+#include"../include/gold.h"
+#include"../include/wood.h"
+
+int main()
+{
+    gold();
+    wood();
+    return 0;
+}
---
+++ class4/src/wood.cpp
@@ -0,0 +1,5 @@
+#include"../include/wood.h"
+void wood()
+{
+    printf("this is wood\r\n");
+}
